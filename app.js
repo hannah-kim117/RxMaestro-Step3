08fbc5900108d1a1047b503c4c25436369773693
Alexander Freddo <freddoa@flip4.engr.oregonstate.edu>
2023-11-16 11:47:48 -0800

    Fixed dropdowns

--- app.js
+++ app.js
@@ -33,8 +33,20 @@ app.get('/manufacturers', function(req, res){
 
 app.get('/patient-prescriptions', function(req, res){
     let query1 = "SELECT patientPrescriptionID, dosage, PatientPrescriptions.drugID, PatientPrescriptions.patientID, drugName, name FROM PatientPrescriptions JOIN Drugs ON PatientPrescriptions.drugID = Drugs.drugID JOIN Patients ON PatientPrescriptions.patientID = Patients.patientID;";
+    let query2 = "SELECT * FROM Patients";
+    let query3 = "SELECT * FROM Drugs";
+
     db.pool.query(query1, function(error, rows, fields) {
-        res.render('PatientPrescriptions', {data: rows});
+        let patientPrescriptions = rows;
+
+        db.pool.query(query2, (error, rows, fields) => {
+            let patients = rows;
+
+            db.pool.query(query3, (error, rows, fields) => {
+                let drugs = rows;
+                return res.render('PatientPrescriptions', {data: patientPrescriptions, patients: patients, drugs: drugs});
+            });
+        });
     });
 });
 
