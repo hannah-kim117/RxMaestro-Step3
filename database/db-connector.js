5c404a9acbbab34de9c577ce4d0a4f12537d140b
Hannah Kim <kimha2@flip1.engr.oregonstate.edu>
2023-11-16 11:27:31 -0800

    no change

--- database/db-connector.js
+++ database/db-connector.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_[your_onid]',
-    password        : '[password]',
-    database        : 'cs340_[your_onid]'
+    user            : 'cs340_[username]',
+    password        : 'password',
+    database        : 'cs340_[username]'
 });
 
 module.exports.pool = pool;
